818978ea56e63999cc6b7be33ce167b1dbab2fd0
Abu Hasan <hasanabubachelor@gmail.com>
2021-10-03 15:27:01 +0600

    Double & Triple assign operator

---
+++ doubke-vs-triple.js
@@ -0,0 +1,16 @@
+// let num1 = 4;
+// let num2  = '4';
+let num1 = 0;
+let num2 = false;
+
+// if(num1 == num2){
+//     console.log("True");
+// }
+// else{
+//     console.log("False");
+// }
+if (num1 === num2) {
+  console.log("True");
+} else {
+  console.log("False");
+}
